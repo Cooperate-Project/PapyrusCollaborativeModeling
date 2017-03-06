4127daf9fa1e1913bbd99318389ef6543a564c5c
Quentin Le Menez <quentin.lemenez@cea.fr>
2017-03-06 16:03:03 +0100

    [releng] [Collaborative Modeling] Populate the git repository
    - Basic architecture
    - targetplatform tailored to Eclipse and papyrus Oxygen
    - Amended headers and added missing ones
    - added the documentation plugin to cdo
    - Added the possibility to run tests even if they fail
    
    Change-Id: I479828eb98569f5208f2685b88f83a33ad04cb8f
    Signed-off-by: Quentin Le Menez <quentin.lemenez@cea.fr>

---
+++ plugins/revision/org.eclipse.papyrus.revisiontool/src/org/eclipse/papyrus/revision/tool/queries/GetStrike.java
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ * Copyright (c) 2012, 2017 CEA LIST.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  (CEA LIST) - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.revision.tool.queries;
+
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.DifferenceKind;
+import org.eclipse.emf.compare.ReferenceChange;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.emf.facet.efacet.core.IFacetManager;
+import org.eclipse.papyrus.emf.facet.query.java.core.IJavaQuery2;
+import org.eclipse.papyrus.emf.facet.query.java.core.IParameterValueList2;
+import org.eclipse.papyrus.revision.tool.core.ReviewResourceManager;
+import org.eclipse.papyrus.revision.tool.ui.ReviewsEditor;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.uml2.uml.Element;
+
+/** This class is query that returns the boolean. It returns true if the element is deleted from the Compare model */
+public class GetStrike implements  IJavaQuery2<EObject, Boolean> {
+	public Boolean evaluate(final EObject context, final IParameterValueList2 parameterValues, final IFacetManager facetManager){
+		if( context instanceof Element){
+			Element element= (Element)context;
+			IWorkbenchPart part=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findView("org.eclipse.papyrus.revisiontool.commentview");
+			if( part instanceof ReviewsEditor){
+				ReviewResourceManager reviewResourceManager=((ReviewsEditor)part).getReviewResourceManager();
+				if(reviewResourceManager.getCurrentReviewModelWithoutLoading()!=null){
+					Comparison comparison=reviewResourceManager.getDiffModel();
+					if(comparison.getDifferences(element).size()>0){
+						if(comparison.getDifferences(element).get(0).getKind().equals(DifferenceKind.DELETE) && isChangeAboutContaiment(comparison, element)){
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	protected boolean isChangeAboutContaiment(Comparison comparison, EObject element){
+		if(comparison.getDifferences(element).get(0) instanceof ReferenceChange){
+			ReferenceChange refChange=(ReferenceChange)comparison.getDifferences(element).get(0);
+			if( refChange.getReference().isContainment()){
+				return true;
+			}
+		}
+		return false;
+	}
+}
